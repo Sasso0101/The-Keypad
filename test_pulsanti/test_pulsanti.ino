e74baeca1a643ba460149bfc78f74a67c17a2434
sasso0101 <salvatore.andaloro01@gmail.com>
2020-07-07 19:10:13 +0200

    Added Arduino code

---
+++ test_pulsanti/test_pulsanti.ino
@@ -0,0 +1,58 @@
+#include <Keypad.h>
+#include "Tlc5940.h"
+
+
+const byte ROWS = 2; //four rows
+const byte COLS = 5; //three columns
+char keys[ROWS][COLS] = {
+  {'0','1','2','3','4'},
+  {'5','6','7','8','9'}
+};
+byte rowPins[ROWS] = {2, 3}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {A0, 8, 7, 6, 4}; //connect to the column pinouts of the keypad
+
+int pins[10] = {7, 6, 5, 9, 8, 2, 1, 0, 15, 14};
+
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup() {
+    Serial.begin(9600);
+    kpd.setHoldTime(500);
+    
+    Tlc.init();
+    Tlc.clear();
+    Tlc.update();
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+      String input = Serial.readString();
+      if (input == "UQzbRPHMVr7JYw") {
+        Serial.write("qrJjhbWQxm5Pbi");
+      }
+      
+    }
+    
+    // Fills kpd.key[ ] array with up-to 10 active keys.
+    // Returns true if there are ANY active keys.
+    if (kpd.getKeys())
+    {
+        for (int i=0; i<LIST_MAX; i++)   // Scan the whole key list.
+        {
+            if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
+            {
+                switch (kpd.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
+                    case PRESSED:
+                    Serial.print(kpd.key[i].kchar);
+                    Serial.print(" ");
+                    Tlc.set(pins[kpd.key[i].kchar - '0'], 4095);
+                    Tlc.update();
+                 break;
+                  case RELEASED:
+                    Tlc.set(pins[kpd.key[i].kchar - '0'], 0);
+                    Tlc.update();
+                }
+            }
+        }
+    }
+}  // End loop
